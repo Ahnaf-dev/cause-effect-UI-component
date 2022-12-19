2b26439bf3679453aa034b2e60b844c3fdaae6ba
Ahnaf-dev <ahnafbari99@gmail.com>
2022-12-19 13:05:26 -0700

    adding hamburger functionality

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 import users from "./data/data.js";
+let sidebar = $(".sidebar");
+let hamburger = $(".hamburger");
+let hamburgerIcon = $("span")[0];
 
 const state = {
   currentUserNumber: 0,
 };
 
+// hamburger menu for mobile to toggle sidebar
+
+hamburger.on("click", () => {
+  sidebar.toggleClass("open");
+
+  if (hamburgerIcon.innerText === "menu") {
+    hamburgerIcon.innerText = "close";
+  } else {
+    hamburgerIcon.innerText = "menu";
+  }
+});
+
+// generating sidebar and display HTML
 $("body").ready(() => {
   let html = users
     .map((user, index) => {
