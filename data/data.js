c5f2fd397321c929cb9271902d81425abd0f785e
Ahnaf-dev <ahnafbari99@gmail.com>
2022-12-19 12:30:39 -0700

    setting up project

---
+++ data/data.js
@@ -0,0 +1,28 @@
+const users = [
+  {
+    name: "Jack Smith",
+    address: "149 ST, EU",
+    about:
+      " I seek the means to fight injustice. To turn fear against those who prey on the fearful. My anger outweights my guilt. Swear to me! Someone like you. Someone who'll rattle the cages.",
+  },
+  {
+    name: "John Cena",
+    address: "100 ST, CA",
+    about:
+      "  I'll be standing where l belong. Between you and the peopIe of Gotham. Accomplice? I'm gonna tell them the whole thing was your idea",
+  },
+  {
+    name: "Douglas Jr",
+    address: "180 ST, EU",
+    about:
+      "  It's ends here. I can't do that as Bruce Wayne... as a man. I'm flesh and blood. I can be ignored, destroyed. But as a symbol, I can be incorruptible, I can be everlasting.",
+  },
+  {
+    name: "Batman",
+    address: "Gotham",
+    about:
+      " I'm Batman I can't do that as Bruce Wayne... as a man. I'm flesh and blood. I can be ignored, destroyed. But as a symbol, I can be incorruptible, I can be everlasting.",
+  },
+];
+
+export default users;
---
+++ index.js
@@ -0,0 +1,4 @@
+import users from "./data/data.js";
+
+console.log(users);
+// $("body").html(`<h1>Hello</h1>`);
